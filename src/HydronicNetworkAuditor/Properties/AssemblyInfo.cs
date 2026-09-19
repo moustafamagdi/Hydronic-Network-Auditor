fb386d24da9ce9a59b578d5527dc241d7cdea4ff
moustafamagdi <moustafa.magdi@gmail.com>
2026-09-19 14:51:17 +0300

    Add assembly metadata

---
+++ src/HydronicNetworkAuditor/Properties/AssemblyInfo.cs
@@ -0,0 +1,11 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Hydronic Network Auditor")]
+[assembly: AssemblyDescription("Revit hydronic network connectivity and logical-system auditor")]
+[assembly: AssemblyCompany("MM Tools")]
+[assembly: AssemblyProduct("Hydronic Network Auditor")]
+[assembly: ComVisible(false)]
+[assembly: Guid("D3DBA139-0FE6-47EF-A295-B2825B8158B1")]
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
